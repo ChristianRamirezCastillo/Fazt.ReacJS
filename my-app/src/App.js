62340d35c3d1d2183a0e64f70400328eccdc4e30
christian <chriserc@outlook.com>
2020-01-19 15:16:43 -0500

    estados

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld(prop) {
+/* function HelloWorld(prop) {
   return (
     <div id="hello">
       <h3>{prop.subittulo}</h3>
       {prop.miTexto}
     </div>
   );
-}
+} */
 
 function App() {
   return (
@@ -21,5 +21,32 @@ function App() {
   );
 }
 
+class HelloWorld extends React.Component {
+  state = {
+    show: true
+  }
+
+  toggleShow = ()=> {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{ this.props.subittulo }</h3>
+          { this.props.miTexto }
+          <button onClick={ this.toggleShow }>Cambiar estado</button>
+        </div>
+      );
+    }else {
+      return <h1>No hay elementos
+        <button onClick={ this.toggleShow }>Cambiar estado</button>
+      </h1>
+    }
+  }
+}
 
 export default App;
